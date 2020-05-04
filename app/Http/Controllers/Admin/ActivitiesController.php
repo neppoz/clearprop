be84b0712a483baa91990f8912d9c024479b76fa
Neves Parrottino <neppoz@hotmail.com>
2020-05-04 10:33:28 +0200

    working version when creating

--- app/Http/Controllers/Admin/ActivitiesController.php
+++ app/Http/Controllers/Admin/ActivitiesController.php
@@ -11,7 +11,10 @@ use App\Plane;
 use App\Type;
 use App\User;
 use Gate;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection as SupportCollection;
+use PhpParser\ErrorHandler\Collecting;
 use Symfony\Component\HttpFoundation\Response;
 use Yajra\DataTables\Facades\DataTables;
 
@@ -103,7 +106,59 @@ class ActivitiesController extends Controller
 
     public function store(StoreActivityRequest $request)
     {
-        $activity = Activity::create($request->all());
+        if ($request->split_cost == false) {
+            $activity = Activity::create($request->all());
+        }
+
+        if ($request->split_cost == true && isset($request->copilot_id)) {
+            $data_pilot = clone $request;
+            $data_copilot = clone $request;
+
+            $counter_split_value = ($request->counter_stop-$request->counter_start)/2;
+            $counter_start_p    = $request->counter_start;
+            $counter_stop_p     = $counter_start_p+$counter_split_value;
+            $counter_start_c    = $counter_stop_p;
+            $counter_stop_c     = $counter_start_c+$counter_split_value;
+
+            $data_copilot->merge([
+                'user_id' => $data_pilot->copilot_id,
+                'copilot_id' => $data_pilot->user_id,
+            ]);
+            $data_pilot->merge([
+                'counter_start' => $counter_start_p,
+                'counter_stop' => $counter_stop_p,
+            ]);
+            $data_copilot->merge([
+                'counter_start' => $counter_start_c,
+                'counter_stop' => $counter_stop_c,
+            ]);
+
+            if ($request->engine_warmup == true) {
+                $warmup_split_value = ($request->counter_start-$request->warmup_start)/2;
+                $warmup_start_p     = $request->warmup_start;
+                $counter_start_p    = $request->warmup_start+$warmup_split_value;
+                $counter_stop_p     = $counter_start_p+$counter_split_value;
+                $warmup_start_c     = $counter_stop_p;
+                $counter_start_c    = $counter_stop_p+$warmup_split_value;
+                $counter_stop_c     = $counter_start_c+$counter_split_value;
+
+                $data_pilot->merge([
+                    'warmup_start' => $warmup_start_p,
+                    'counter_start' => $counter_start_p,
+                    'counter_stop' => $counter_stop_p,
+                ]);
+
+                $data_copilot->merge([
+                    'warmup_start' => $warmup_start_c,
+                    'counter_start' => $counter_start_c,
+                    'counter_stop' => $counter_stop_c,
+                ]);
+            };
+
+            /** Create two records */
+            $activity = Activity::create($data_pilot->all());
+            $activity = Activity::create($data_copilot->all());
+        }
 
         return redirect()->route('admin.activities.index');
     }
