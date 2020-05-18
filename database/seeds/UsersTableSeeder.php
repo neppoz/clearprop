04769a06708b936ab13bbbc1e7251fc9263b4abc
Neves Parrottino <neppoz@hotmail.com>
2020-05-18 15:51:58 +0200

    seed column lang in users table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,6 +15,7 @@ class UsersTableSeeder extends Seeder
                 'email'          => 'neppoz.com@gmail.com',
                 'password'       => '$2y$10$XzjqHC9sHvOgxSN9Xk7A7.KSmh/PUZztf/MOqZUgDGT5rB5vYQ/9C',
                 'remember_token' => null,
+                'lang'           => 'EN',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
