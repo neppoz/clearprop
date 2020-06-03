348c9326a335d01317ca2775e0b7cde249ccb04b
Neves Parrottino <neppoz@hotmail.com>
2020-06-03 17:52:25 +0200

    plain laravel markdown email template

---
+++ resources/views/emails/users/report.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+
+Caro socio,<br>
+<br>
+in allegato trovi il resoconto della tua situazione economica.<br>
+<br>
+
+{{-- @component('mail::button', ['url' => ''])
+Button Text
+@endcomponent --}}
+
+Cordiali saluti,<br>
+Il direttivo ASD PUMA
+@endcomponent
