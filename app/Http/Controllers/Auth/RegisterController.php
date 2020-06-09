1d9dda9a63d6d921f3e1173cb488803f1d257f17
Neves Parrottino <neppoz@hotmail.com>
2020-06-09 18:57:59 +0200

    redirect to register-step2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/register-step2';
 
     /**
      * Create a new controller instance.
