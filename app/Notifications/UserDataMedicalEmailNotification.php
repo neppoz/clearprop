70389cf6eb540e895d64d8c92319e09625f6d715
Neves Parrottino <neppoz@hotmail.com>
2020-07-02 18:10:29 +0200

    disabled queue for notification

--- app/Notifications/UserDataMedicalEmailNotification.php
+++ app/Notifications/UserDataMedicalEmailNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\HtmlString;
 
-class UserDataMedicalEmailNotification extends Notification implements ShouldQueue
+class UserDataMedicalEmailNotification extends Notification
 {
     use Queueable;
 
