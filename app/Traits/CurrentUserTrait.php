ec91c5c95a66ae83de04f4f965326a4b00d537bf
Neves Parrottino <neppoz@hotmail.com>
2020-07-11 10:56:16 +0200

    filtering user records by trait

---
+++ app/Traits/CurrentUserTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+trait CurrentUserTrait
+{
+    public static function bootCurrentUserTrait()
+    {
+        if (!app()->runningInConsole() && auth()->check()) {
+            $isAdmin = auth()->user()->roles->contains(1);
+            // static::creating(function ($model) use ($isAdmin) {
+            //     // Prevent admin from setting his own id - admin entries are global.
+
+            //     // If required, remove the surrounding IF condition and admins will act as users
+            //     if (!$isAdmin) {
+            //         $model->created_by_id = auth()->id();
+            //     }
+            // });
+            if (!$isAdmin) {
+                static::addGlobalScope('user_id', function (Builder $builder) {
+                    $builder->where('user_id', auth()->id());
+                });
+            }
+        }
+    }
+}
