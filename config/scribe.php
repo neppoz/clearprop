038346970bbc65ec6a344566d39a2ff0e62888c5
Neves Parrottino <neppoz@hotmail.com>
2020-07-11 11:15:01 +0200

    config change

--- config/scribe.php
+++ config/scribe.php
@@ -6,7 +6,7 @@ return [
      * - "static" will generate a static HTMl page in the /public/docs folder,
      * - "laravel" will generate the documentation as a Blade view, so you can add routing and authentication.
      */
-    'type' => 'laravel',
+    'type' => 'static',
 
     /*
      * Settings for `static` type output.
@@ -79,7 +79,7 @@ return [
      * Text to place in the "Introduction" section. Markdown and HTML are supported.
      */
     'intro_text' => <<<INTRO
-Welcome to our API documentation for ClearProp!
+Welcome to our ClearProp API documentation!
 
 <aside>As you scroll, you'll see code examples for working with the API in different programming languages in the dark area to the right (or as part of the content on mobile), and you can switch the programming language of the examples with the tabs in the top right (or from the nav menu at the top left on mobile).</aside>
 INTRO
