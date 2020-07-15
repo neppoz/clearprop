a7e68eaa30ab9f10922f4ed9c3a57c5bd739037c
Neves Parrottino <neppoz@hotmail.com>
2020-07-15 12:10:03 +0200

    install laravel sanctum

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
@@ -16,6 +17,7 @@ class Kernel extends HttpKernel
 
     protected $middlewareGroups = [
         'api' => [
+            EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             'bindings',
             \App\Http\Middleware\AuthGates::class,
