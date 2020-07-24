cdaa9d83813451c850190b70d271e6727c6af164
Neves Parrottino <neppoz@hotmail.com>
2020-07-24 13:35:05 +0200

    wip

--- app/Listeners/BookingCreatedListener.php
+++ app/Listeners/BookingCreatedListener.php
@@ -2,10 +2,16 @@
 
 namespace App\Listeners;
 
+use App\Events\BookingCreatedEvent;
+use App\Notifications\BookingDataCreateEmailNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Notification;
+use Throwable;
+use App\User;
+use App\Plane;
 
-class BookingCreatedListener
+class BookingCreatedListener implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -23,10 +29,24 @@ class BookingCreatedListener
      * @param  object  $event
      * @return void
      */
-    public function handle($event)
+    public function handle(BookingCreatedEvent $event)
     {
-        // TODO
-        // $data  = ['action' => 'created', 'model_name' => 'Booking'];
-        //Notification::send($users, new DataChangeEmailNotification($data));
+        try {
+            $users = User::findOrFail($event->booking->user_id);
+            $plane = Plane::findOrFail($event->booking->plane_id);
+
+            /** Recipients */
+            $admins = User::wherehas('roles', function ($q) {
+                $q->where('role_id', User::IS_ADMIN);
+            })->get();
+
+            $data  = ['reservation_start' => $event->booking->reservation_start, 'reservation_stop' => $event->booking->reservation_stop];
+            Notification::send($users, new BookingDataCreateEmailNotification($data));
+            Notification::send($admins, new BookingDataCreateEmailNotification($data));
+            return;
+        } catch (Throwable $exception) {
+            report($exception);
+            return back()->withToastError($exception->getMessage());
+        }
     }
 }
