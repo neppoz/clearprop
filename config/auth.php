237333a1ad9adc279f1893eb465d95dd787fdb08
Neves Parrottino <neppoz@hotmail.com>
2020-09-16 16:02:12 +0200

    cors configuration keys

--- config/auth.php
+++ config/auth.php
@@ -42,9 +42,8 @@ return [
         ],
 
         'api' => [
-            'driver'   => 'passport',
+            'driver'   => 'session',
             'provider' => 'users',
-            'hash'     => false,
         ],
     ],
 
