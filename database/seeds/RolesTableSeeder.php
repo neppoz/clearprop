ddda9a5f3a796d9de63a57796db90c93c7092ebf
Neves Parrottino <neppoz@hotmail.com>
2020-09-16 17:57:58 +0200

    seeder updates

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -21,6 +21,12 @@ class RolesTableSeeder extends Seeder
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
+            [
+                'id'    => 3,
+                'title' => 'Manager',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
         ];
 
         Role::insert($roles);
