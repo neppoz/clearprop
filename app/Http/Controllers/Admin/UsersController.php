8143a28ac8864c84084a2f2d1c3a336c3dc8ccdd
Neves Parrottino <neppoz@hotmail.com>
2020-09-17 08:42:01 +0200

    controllers fix

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Factor;
+use App\Plane;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MassDestroyUserRequest;
 use App\Http\Requests\StoreUserRequest;
