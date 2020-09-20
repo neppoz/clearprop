6a5e38ee0b1c9060b014506df26bfeb7f53c685f
Neves Parrottino <neppoz@hotmail.com>
2020-09-20 15:30:37 +0200

    bugsnag config

--- config/app.php
+++ config/app.php
@@ -229,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Bugsnag' => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
 
     ],
 
