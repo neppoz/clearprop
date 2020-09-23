c8fa6fb613e0581f096743a26d6063b661f1228d
Neves Parrottino <neppoz@hotmail.com>
2020-09-23 13:25:03 +0200

    migrations and permissions for slots

---
+++ database/migrations/2020_09_23_000006_create_slots_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Permission;
+use App\Role;
+use App\User;
+
+class CreateSlotsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropForeign('bookings_ibfk_5');
+            $table->dropColumn('type_id');
+        });
+
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->unsignedInteger('slot_id')->after('user_id')->nullable();
+            $table->foreign('slot_id', 'slot_fk_bookings')->references('id')->on('slots');
+        });
+
+        $permissions = [
+            [
+                'id' => '77',
+                'title' => 'slot_access',
+            ],
+            [
+                'id' => '78',
+                'title' => 'schedule_access',
+            ],
+        ];
+        Permission::insertOrIgnore($permissions);
+
+        $admin_permissions = Permission::all();
+        Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
+
+    }
+}
