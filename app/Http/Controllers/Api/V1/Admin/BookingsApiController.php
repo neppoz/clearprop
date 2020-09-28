9c1366b311427e7c2afd110f2ceca65176aafc8c
Neves Parrottino <neppoz@hotmail.com>
2020-09-28 07:50:03 +0200

    api changes

--- app/Http/Controllers/Api/V1/Admin/BookingsApiController.php
+++ app/Http/Controllers/Api/V1/Admin/BookingsApiController.php
@@ -24,7 +24,7 @@ class BookingsApiController extends Controller
     {
 //        abort_if(Gate::denies('booking_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $bookings = Booking::with(['user', 'plane', 'created_by'])->get();
+        $bookings = Booking::with(['user', 'plane', 'created_by'])->orderBy('reservation_start')->paginate(10);
 
         return BookingResource::collection($bookings);
     }
