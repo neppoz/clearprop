d5620dd268b209d22702ffde5af02244253db260
Neves Parrottino <neppoz@hotmail.com>
2020-10-05 13:06:47 +0200

    Dashboard resource

---
+++ app/Http/Resources/Admin/DashboardResource.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DashboardResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+
+    }
+}
+
