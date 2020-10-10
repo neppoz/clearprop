e24e2b4a4b74f6b9c620b95372fbdc29a00b8118
Neves Parrottino <neppoz@hotmail.com>
2020-10-10 10:54:19 +0200

    first basic working payment

--- app/Http/Controllers/Pilot/CheckoutController.php
+++ app/Http/Controllers/Pilot/CheckoutController.php
@@ -15,13 +15,13 @@ class CheckoutController extends Controller
         try {
             \Stripe\Stripe::setApiKey('sk_test_51HY3SMKi5dM1ECAoEIARwLvPyljsAMlqCt3rNgP0vClpnAx5WLgcWSaPS0udLNVJSXPb7dDCz5OXkPksxH3R1STz00GTukXV2O');
 
-            $paymentIntent = \Stripe\PaymentIntent::create([
-                'amount' => $request->input('amount'),
+            $intent = \Stripe\PaymentIntent::create([
+                'amount' => $request->input('amount') * 100,
                 'currency' => 'eur',
                 'metadata' => ['integration_check' => 'accept_a_payment'],
             ]);
 
-            return view('pilot.billings.checkout', compact('paymentIntent'));
+            return view('pilot.billings.checkout', compact('intent'));
 
         } catch (Error $e) {
             http_response_code(500);
@@ -29,43 +29,49 @@ class CheckoutController extends Controller
         }
     }
 
-    public function charge(Request $request)
+    public function processCheckout(Request $request)
     {
-        return view('pilot.billings.index');
-
-
+        dd($request);
     }
-
-    public function calculateBasket(Request $request)
-    {
-        // Get itmes from DB and Pass to view
-        $items = [
-            'id' => 'Acconto voli',
-            'amount' => '400',
-        ];
-
-        return view('pilot.billings.checkout', compact('items'));
-    }
-
-    public function cardCheckout(Request $request)
-    {
-        try {
-            \Stripe\Stripe::setApiKey('sk_test_51HY3SMKi5dM1ECAoEIARwLvPyljsAMlqCt3rNgP0vClpnAx5WLgcWSaPS0udLNVJSXPb7dDCz5OXkPksxH3R1STz00GTukXV2O');
-
-            $paymentIntent = \Stripe\PaymentIntent::create([
-                'amount' => '400',
-                'currency' => 'eur',
-            ]);
-            $output = [
-                'clientSecret' => $paymentIntent->client_secret,
-            ];
-
-            return json_encode($output);
-
-        } catch (Error $e) {
-            http_response_code(500);
-            return json_encode(['error' => $e->getMessage()]);
-        }
-    }
+//
+//    public function charge(Request $request)
+//    {
+//
+//        return view('pilot.billings.index');
+//
+//
+//    }
+//
+//    public function calculateBasket(Request $request)
+//    {
+//        // Get itmes from DB and Pass to view
+//        $items = [
+//            'id' => 'Acconto voli',
+//            'amount' => '400',
+//        ];
+//
+//        return view('pilot.billings.checkout', compact('items'));
+//    }
+//
+//    public function cardCheckout(Request $request)
+//    {
+//        try {
+//            \Stripe\Stripe::setApiKey('sk_test_51HY3SMKi5dM1ECAoEIARwLvPyljsAMlqCt3rNgP0vClpnAx5WLgcWSaPS0udLNVJSXPb7dDCz5OXkPksxH3R1STz00GTukXV2O');
+//
+//            $paymentIntent = \Stripe\PaymentIntent::create([
+//                'amount' => '400',
+//                'currency' => 'eur',
+//            ]);
+//            $output = [
+//                'clientSecret' => $paymentIntent->client_secret,
+//            ];
+//
+//            return json_encode($output);
+//
+//        } catch (Error $e) {
+//            http_response_code(500);
+//            return json_encode(['error' => $e->getMessage()]);
+//        }
+//    }
 
 }
--- resources/views/pilot/billings/index.blade.php
+++ resources/views/pilot/billings/index.blade.php
@@ -22,34 +22,56 @@
                 <div class="col-md-6">
                     <form method="POST" action="{{ route("pilot.checkout.paymentIntent") }}" id="payment-form">
                         @csrf
+
+                        {{--                        <div class="form-group">--}}
+                        {{--                            <label class="required" for="amount">{{ trans('cruds.billing.fields.amount') }}</label>--}}
+                        {{--                            <select class="form-control select2 {{ $errors->has('amount') ? 'is-invalid' : '' }}"--}}
+                        {{--                                    name="amount" id="amount" required>--}}
+                        {{--                                <option value="100" selected>100 €</option>--}}
+                        {{--                                <option value="200">200 €</option>--}}
+                        {{--                                <option value="300">300 €</option>--}}
+                        {{--                                <option value="400">400 €</option>--}}
+                        {{--                                <option value="500">500 €</option>--}}
+                        {{--                            </select>--}}
+                        {{--                            @if($errors->has('value'))--}}
+                        {{--                                <span class="text-danger">{{ $errors->first('amount') }}</span>--}}
+                        {{--                            @endif--}}
+                        {{--                        </div>--}}
+
+                        {{--                        <label for="card-element">--}}
+                        {{--                            Credit or debit card--}}
+                        {{--                        </label>--}}
+                        {{--                        <div id="card-element">--}}
+                        {{--                            <!-- A Stripe Element will be inserted here. -->--}}
+                        {{--                        </div>--}}
+
+                        {{--                        <!-- Used to display form errors. -->--}}
+                        {{--                        <div id="card-errors" role="alert"></div>--}}
+                        <div class="form-group">
+                            <label class="required" for="title">{{ trans('cruds.billing.fields.title') }}</label>
+                            <input class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" type="text"
+                                   name="title"
+                                   id="title" value="{{ old('title', 'Versamento acconto') }}" required>
+                            @if($errors->has('title'))
+                                <span class="text-danger">{{ $errors->first('title') }}</span>
+                            @endif
+                            <span
+                                class="help-block help-block text-secondary small">{{ trans('cruds.billing.fields.amount_helper') }}</span>
+                        </div>
                         <div class="form-group">
                             <label class="required" for="amount">{{ trans('cruds.billing.fields.amount') }}</label>
-                            <select class="form-control select2 {{ $errors->has('amount') ? 'is-invalid' : '' }}"
-                                    name="amount" id="amount" required>
-                                <option value="100" selected>100 €</option>
-                                <option value="200">200 €</option>
-                                <option value="300">300 €</option>
-                                <option value="400">400 €</option>
-                                <option value="500">500 €</option>
-                            </select>
-                            @if($errors->has('value'))
+                            <input class="form-control {{ $errors->has('amount') ? 'is-invalid' : '' }}" type="number"
+                                   name="amount"
+                                   id="amount" value="{{ old('amount', '150') }}" step="0.01" required>
+                            @if($errors->has('amount'))
                                 <span class="text-danger">{{ $errors->first('amount') }}</span>
                             @endif
+                            <span
+                                class="help-block help-block text-secondary small">{{ trans('cruds.billing.fields.amount_helper') }}</span>
                         </div>
-
-                        <label for="card-element">
-                            Credit or debit card
-                        </label>
-                        <div id="card-element">
-                            <!-- A Stripe Element will be inserted here. -->
-                        </div>
-
-                        <!-- Used to display form errors. -->
-                        <div id="card-errors" role="alert"></div>
-
                         <div class="form-group">
                             <button class="btn btn-primary" type="submit">
-                                Paga con Carta di credito
+                                Acquista con Carta di credito
                             </button>
                         </div>
                     </form>
