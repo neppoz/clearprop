a5cf1a8a645b6da458f1c936127f68114a7bd039
Neves Parrottino <neppoz@hotmail.com>
2020-10-14 18:25:26 +0200

    layout adjust

--- resources/views/pilot/billings/checkout.blade.php
+++ resources/views/pilot/billings/checkout.blade.php
@@ -24,25 +24,18 @@
                     <div class="h2">{{number_format($intent->amount / 100, 2,',','.')}} &euro;</div>
 
                     <div class="row align-items-center mt-2 bg-light">
-                        <div class="col mx-auto">
+                        <div class="col-sm-2 col-md-4 mx-auto">
                             <img src="{{asset('/images/payment/1.png')}}" alt="visa">
                         </div>
-                        <div class="col mx-auto">
-                            <img src="{{asset('/images/payment/2.png')}}" alt="mastercard">
-                        </div>
-                        {{--                        <div class="col mx-auto">--}}
-                        {{--                            <img src="{{asset('/images/payment/3.png')}}" alt="maestro">--}}
-                        {{--                        </div>--}}
-                        {{--                        <div class="col mx-auto">--}}
-                        {{--                            <img src="{{asset('/images/payment/4.png')}}" alt="cirrus">--}}
-                        {{--                        </div>--}}
-                    </div>
-                    <div class="row align-items-center mb-2 bg-light">
-                        <div class="col-12 text-center">
+                        <div class="col-sm-2 col-md-4 mx-auto text-center">
                             <i class="fas fa-check-circle text-success"></i>
                             <p class="text-success text-bold">{{trans('global.secure-transaction')}}</p>
                         </div>
+                        <div class="col-sm-2 col-md-4 mx-auto">
+                            <img src="{{asset('/images/payment/2.png')}}" alt="mastercard">
+                        </div>
                     </div>
+
                 </div>
             </div>
             <div class="row align-items-center mt-2 border-secondary">
@@ -55,36 +48,16 @@
                         <input type="hidden" name="payment-status" id="payment-status" value=""/>
                         <h5>{{trans('cruds.checkout.fields.card-element')}}</h5>
                         <hr>
-                        {{--                        <div class="form-group">--}}
-                        {{--                            <input id="card-holder-name" type="text" placeholder="{{trans('cruds.checkout.fields.cardholder-name')}}"/>--}}
-                        {{--                            <div id="card-element" class="form-control">--}}
-                        {{--                                <!-- Elements will create input elements here -->--}}
-                        {{--                            </div>--}}
-                        {{--                        </div>--}}
-                        {{--                        <!-- We'll put the error messages in this element -->--}}
-                        {{--                        <div id="card-errors" role="alert"></div>--}}
-                        {{--    --}}
-                        {{--                        <label class="font-weight-light" for="card-name">{{trans('cruds.checkout.fields.cardholder-name')}}</label>--}}
                         <div class="input-group mb-2">
                             <input type="text" class="form-control" id="card-name"
                                    placeholder="{{trans('cruds.checkout.fields.cardholder-name')}}" required>
                         </div>
-                        {{--                        <label for="card-number">Credit Card Number</label>--}}
                         <div class="input-group mb-2">
-                            {{--                            <div class="input-group-prepend">--}}
-                            {{--                                <span class="input-group-text">C</span>--}}
-                            {{--                            </div>--}}
                             <span id="card-number" class="form-control">
                             <!-- Stripe Card Element -->
                             </span>
-                            {{--                            <div class="input-group-append">--}}
-                            {{--                                <span class="input-group-text">D</span>--}}
-                            {{--                            </div>--}}
                         </div>
                         <div class="input-group mb-2">
-                            {{--                            <div class="input-group-prepend">--}}
-                            {{--                                <span class="input-group-text">E</span>--}}
-                            {{--                            </div>--}}
                             <span id="card-cvc" class="form-control">
                             <!-- Stripe CVC Element -->
                             </span>
@@ -92,19 +65,10 @@
                             <!-- Stripe Card Expiry Element -->
                             </span>
                         </div>
-                        {{--                        <label for="card-exp">Expiration</label>--}}
-                        {{--                        <div class="input-group mb-2">--}}
-                        {{--                        <span id="card-exp" class="form-control">--}}
-                        {{--                            <!-- Stripe Card Expiry Element -->--}}
-                        {{--                        </span>--}}
-                        {{--                            <div class="input-group-append">--}}
-                        {{--                                <span class="input-group-text">F</span>--}}
-                        {{--                            </div>--}}
-                        {{--                        </div>--}}
+
                         <br>
                         <div class="form-group text-center">
-                            <button class="btn btn-success" id="checkout-button">
-                                {{--                                <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>--}}
+                            <button class="btn btn-block btn-success" id="checkout-button">
                                 <i class="fas fa-lock"></i>
                                 {{trans('global.pay-now')}}
                             </button>
@@ -142,23 +106,6 @@
                 stripeAccount: "{{ env('STRIPE_CONNECTED_ACCOUNT') }}",
             })
             let elements = stripe.elements()
-            // let style = {
-            //     base: {
-            //         color: '#32325d',
-            //         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-            //         fontSmoothing: 'antialiased',
-            //         fontSize: '16px',
-            //         '::placeholder': {
-            //             color: '#aab7c4'
-            //         }
-            //     },
-            //     invalid: {
-            //         color: '#fa755a',
-            //         iconColor: '#fa755a'
-            //     }
-            // }
-            // let card = elements.create('card', {style: style})
-            // card.mount('#card-element')
 
             // Try to match bootstrap 4 styling
             var style = {
