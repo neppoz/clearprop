c6dd06b2343b2cad788e781923be674c93013854
Neves Parrottino <neppoz@hotmail.com>
2020-10-16 09:20:27 +0200

    create reservation instructor dropdown added

--- resources/views/admin/bookings/create.blade.php
+++ resources/views/admin/bookings/create.blade.php
@@ -102,6 +102,22 @@
                         class="help-block text-secondary small">{!! trans('cruds.booking.fields.instructor_needed_helper') !!}</span>
                 </div>
                 <div class="form-group">
+                    <label for="instructor_id">{{ trans('cruds.booking.fields.instructor') }}</label>
+                    <select class="form-control select2 {{ $errors->has('instructor') ? 'is-invalid' : '' }}"
+                            name="instructor_id"
+                            id="instructor_id">
+                        @foreach($instructors as $id => $instructor)
+                            <option
+                                value="{{ $id }}" {{ old('instructor_id') == $id ? 'selected' : '' }}>{{ $instructor }}</option>
+                        @endforeach
+                    </select>
+                    @if($errors->has('instructor'))
+                        <span class="text-danger">{{ $errors->first('instructor') }}</span>
+                    @endif
+                    <span
+                        class="help-block text-secondary small">{{ trans('cruds.booking.fields.instructor_helper') }}</span>
+                </div>
+                <div class="form-group">
                     <div class="alert alert-warning alert-dismissible" id="warning-medical" style="display: none">
                         <h5><i class="icon fas fa-info"></i>{{ trans('global.caution') }}</h5>
                         {!! trans('global.medicalCheck_for_admin') !!}
