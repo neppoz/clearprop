81a7fd2d70b01bfa11d9963e7f96a6dd168f30da
Neves Parrottino <neppoz@hotmail.com>
2020-10-21 11:49:51 +0200

    update pilot profile fields enhancements

--- resources/views/auth/passwords/edit.blade.php
+++ resources/views/auth/passwords/edit.blade.php
@@ -32,6 +32,74 @@
                             @endif
                         </div>
                         <div class="form-group">
+                            <label class="required" for="license">{{ trans('cruds.user.fields.license') }}</label>
+                            <input class="form-control {{ $errors->has('license') ? 'is-invalid' : '' }}" type="text"
+                                   name="license" id="license" value="{{ old('license', auth()->user()->license) }}"
+                                   required>
+                            @if($errors->has('license'))
+                                <span class="text-danger">{{ $errors->first('license') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.user.fields.license_helper') }}</span>
+                        </div>
+                        <div class="form-group">
+                            <label class="required"
+                                   for="medical_due">{{ trans('cruds.user.fields.medical_due') }}</label>
+                            <input class="form-control date {{ $errors->has('medical_due') ? 'is-invalid' : '' }}"
+                                   type="text" name="medical_due" id="medical_due"
+                                   value="{{ old('medical_due', auth()->user()->medical_due) }}" required>
+                            @if($errors->has('medical_due'))
+                                <span class="text-danger">{{ $errors->first('medical_due') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.user.fields.medical_due_helper') }}</span>
+                        </div>
+                        <div class="form-group">
+                            <label class="required" for="taxno">{{ trans('cruds.user.fields.taxno') }}</label>
+                            <input class="form-control {{ $errors->has('taxno') ? 'is-invalid' : '' }}" type="text"
+                                   name="taxno" id="taxno" value="{{ old('taxno', auth()->user()->taxno) }}" required>
+                            @if($errors->has('taxno'))
+                                <span class="text-danger">{{ $errors->first('taxno') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.user.fields.taxno_helper') }}</span>
+                        </div>
+                        <div class="form-group">
+                            <label class="required" for="phone_1">{{ trans('cruds.user.fields.phone_1') }}</label>
+                            <input class="form-control {{ $errors->has('phone_1') ? 'is-invalid' : '' }}" type="text"
+                                   name="phone_1" id="phone_1" value="{{ old('phone_1', auth()->user()->phone_1) }}"
+                                   required>
+                            @if($errors->has('phone_1'))
+                                <span class="text-danger">{{ $errors->first('phone_1') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.user.fields.phone_1_helper') }}</span>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone_2">{{ trans('cruds.user.fields.phone_2') }}</label>
+                            <input class="form-control {{ $errors->has('phone_2') ? 'is-invalid' : '' }}" type="text"
+                                   name="phone_2" id="phone_2" value="{{ old('phone_2', auth()->user()->phone_2) }}">
+                            @if($errors->has('phone_2'))
+                                <span class="text-danger">{{ $errors->first('phone_2') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.user.fields.phone_2_helper') }}</span>
+                        </div>
+                        <div class="form-group">
+                            <label class="required" for="address">{{ trans('cruds.user.fields.address') }}</label>
+                            <input class="form-control {{ $errors->has('address') ? 'is-invalid' : '' }}" type="text"
+                                   name="address" id="address" value="{{ old('address', auth()->user()->address) }}"
+                                   required>
+                            @if($errors->has('address'))
+                                <span class="text-danger">{{ $errors->first('address') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.user.fields.address_helper') }}</span>
+                        </div>
+                        <div class="form-group">
+                            <label class="required" for="city">{{ trans('cruds.user.fields.city') }}</label>
+                            <input class="form-control {{ $errors->has('city') ? 'is-invalid' : '' }}" type="text"
+                                   name="city" id="city" value="{{ old('city', auth()->user()->city) }}" required>
+                            @if($errors->has('city'))
+                                <span class="text-danger">{{ $errors->first('city') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.user.fields.city_helper') }}</span>
+                        </div>
+                        <div class="form-group">
                             <button class="btn btn-danger" type="submit">
                                 {{ trans('global.save') }}
                             </button>
