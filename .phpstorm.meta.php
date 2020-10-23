b6a1ca78d3f8adb68643e1b522eba893ca32cb99
Neves Parrottino <neppoz@hotmail.com>
2020-10-23 12:50:09 +0200

    install spatie/laravel-translatable

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-10-13 09:24:11.
+    * Generated on 2020-10-23 10:45:39.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
