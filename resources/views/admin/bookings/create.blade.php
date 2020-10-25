2c1766c2878a0bb1a51592aab77fed95d805e94d
Neves Parrottino <neppoz@hotmail.com>
2020-10-25 19:41:51 +0100

    layouts for slots

--- resources/views/admin/bookings/create.blade.php
+++ resources/views/admin/bookings/create.blade.php
@@ -23,33 +23,49 @@
                 @csrf
                 <div class="form-group">
                     <label class="required" for="mode_id">{{ trans('cruds.booking.fields.mode') }}</label>
-                    <select class="form-control select2 {{ $errors->has('mode') ? 'is-invalid' : '' }}" name="mode_id"
-                            id="mode_id" required>
-                        @foreach($modes as $id => $mode)
-                            <option
-                                value="{{ $id }}" {{ old('mode_id') == $id ? 'selected' : '' }}>{{ $mode }}</option>
-                        @endforeach
-                    </select>
-                    @if($errors->has('mode'))
-                        <span class="text-danger">{{ $errors->first('mode') }}</span>
-                    @endif
-                    <span
-                        class="help-block text-secondary small">{{ trans('cruds.mode.fields.mode_helper') }}</span>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">
+                            <i class="fas fa-tags"></i>
+                          </span>
+                        </div>
+                        <select class="form-control select2 {{ $errors->has('mode') ? 'is-invalid' : '' }}"
+                                name="mode_id"
+                                id="mode_id" required>
+                            @foreach($modes as $id => $mode)
+                                <option
+                                    value="{{ $id }}" {{ old('mode_id') == $id ? 'selected' : '' }}>{{ $mode }}</option>
+                            @endforeach
+                        </select>
+                        @if($errors->has('mode'))
+                            <span class="text-danger">{{ $errors->first('mode') }}</span>
+                        @endif
+                        <span
+                            class="help-block text-secondary small">{{ trans('cruds.booking.fields.mode_helper') }}</span>
+                    </div>
                 </div>
                 <div class="form-group">
                     <label class="required" for="plane_id">{{ trans('cruds.booking.fields.plane') }}</label>
-                    <select class="form-control select2 {{ $errors->has('plane') ? 'is-invalid' : '' }}" name="plane_id"
-                            id="plane_id" required>
-                        @foreach($planes as $id => $plane)
-                            <option
-                                value="{{ $id }}" {{ old('plane_id') == $id ? 'selected' : '' }}>{{ $plane }}</option>
-                        @endforeach
-                    </select>
-                    @if($errors->has('plane'))
-                        <span class="text-danger">{{ $errors->first('plane') }}</span>
-                    @endif
-                    <span
-                        class="help-block text-secondary small">{{ trans('cruds.booking.fields.plane_helper') }}</span>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">
+                            <i class="fas fa-plane"></i>
+                          </span>
+                        </div>
+                        <select class="form-control select2 {{ $errors->has('plane') ? 'is-invalid' : '' }}"
+                                name="plane_id"
+                                id="plane_id" required>
+                            @foreach($planes as $id => $plane)
+                                <option
+                                    value="{{ $id }}" {{ old('plane_id') == $id ? 'selected' : '' }}>{{ $plane }}</option>
+                            @endforeach
+                        </select>
+                        @if($errors->has('plane'))
+                            <span class="text-danger">{{ $errors->first('plane') }}</span>
+                        @endif
+                        <span
+                            class="help-block text-secondary small">{{ trans('cruds.booking.fields.plane_helper') }}</span>
+                    </div>
                 </div>
                 <div class="form-group">
                     <label class="required"
