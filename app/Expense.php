048358f4727739edce06f4b2aca2b044c76196bb
Neves Parrottino <neppoz@hotmail.com>
2020-10-29 12:36:16 +0100

    CurrentUserTrait no needed on Expenses

--- app/Expense.php
+++ app/Expense.php
@@ -37,7 +37,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Expense extends Model
 {
-    use SoftDeletes, CurrentUserTrait;
+    use SoftDeletes;
 
     public $table = 'expenses';
 
