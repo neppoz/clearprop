825c06a380f1762ecc91e644cdba46cf9fbdfc5d
Neves Parrottino <neppoz@hotmail.com>
2021-06-08 12:29:09 +0200

    fix: activities of all members not shown to manager roles

--- app/Traits/CurrentUserTrait.php
+++ app/Traits/CurrentUserTrait.php
@@ -4,13 +4,14 @@ namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 trait CurrentUserTrait
 {
     public static function bootCurrentUserTrait()
     {
         if (!app()->runningInConsole() && auth()->check()) {
-            $isAdmin = auth()->user()->is_admin;
+
             // static::creating(function ($model) use ($isAdmin) {
             //     // Prevent admin from setting his own id - admin entries are global.
 
@@ -19,7 +20,8 @@ trait CurrentUserTrait
             //         $model->created_by_id = auth()->id();
             //     }
             // });
-            if (!$isAdmin) {
+
+            if (auth()->user()->roles->contains(User::IS_MEMBER)) {
                 static::addGlobalScope('user_id', function (Builder $builder) {
                     $builder->where('user_id', auth()->id());
                 });
