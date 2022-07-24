9508b8c2171e2cd19af5e07650cd5643ed543fc7
Neves Parrottino <neppoz@hotmail.com>
2022-07-24 19:05:42 +0200

    change: aligning admin interface, preparing for unify layouts

--- app/Http/Controllers/Pilot/HomeController.php
+++ app/Http/Controllers/Pilot/HomeController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers\Pilot;
 
 use App\Booking;
+use App\Parameter;
 use App\Services\StatisticsService;
-use App\Slot;
-use Illuminate\Support\Str;
-use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -15,6 +14,15 @@ class HomeController
     public function index(Request $request)
     {
         $statistics = (new StatisticsService())->dashboard($request);
+        if (Parameter::where('slug', 'check.medical')->value('value') == Parameter::CHECK_MEDICAL_ENABLED) {
+            $userMedicalGoingDue = User::whereNotNull('medical_due')
+                ->whereBetween('medical_due', [Carbon::now(), Carbon::now()->addDays(40)])
+                ->orWhere('medical_due', '<', [Carbon::now(), Carbon::now()])
+                ->orderBy('medical_due', 'desc')
+                ->get();
+        } else {
+            $userMedicalGoingDue = '';
+        }
 
         $bookingDates = Booking::with(['plane', 'bookingUsers', 'bookingInstructors', 'slot', 'mode'])
             ->where('reservation_start', '>=', Carbon::parse(today()))
@@ -38,7 +46,7 @@ class HomeController
                 return $booking->slot->title . ' - ' . Carbon::createFromFormat('d/m/Y H:i', $booking->reservation_start)->isoFormat('dddd, DD MMMM YYYY');
             });
 
-        return view('welcome', compact('statistics', 'bookingDates', 'checkinDates'));
+        return view('welcome', compact('statistics', 'bookingDates', 'checkinDates', 'userMedicalGoingDue'));
     }
 
 }
