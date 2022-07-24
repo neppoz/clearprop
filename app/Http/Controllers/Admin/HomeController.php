0706a4f587ae50800d34e545501b4e0e3fd1560a
Neves Parrottino <neppoz@hotmail.com>
2022-07-24 19:05:42 +0200

    change: aligning admin interface, preparing for unify layouts

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Booking;
 use App\Parameter;
 use App\Services\StatisticsService;
 use App\User;
@@ -23,7 +24,29 @@ class HomeController
             $userMedicalGoingDue = '';
         }
 
-        return view('home', compact('statistics', 'userMedicalGoingDue'));
+        $bookingDates = Booking::with(['plane', 'bookingUsers', 'bookingInstructors', 'slot', 'mode'])
+            ->where('reservation_start', '>=', Carbon::parse(today()))
+            ->orderBy('reservation_start', 'asc')
+            ->get()
+            ->groupBy(function ($booking) {
+                return Carbon::createFromFormat('d/m/Y H:i', $booking->reservation_start)->isoFormat('ddd DD MMM');
+            });
+
+        $checkinDates = Booking::with(['plane', 'bookingUsers', 'bookingInstructors', 'slot'])
+            ->whereDoesntHave('bookingUsers', function ($query) {
+                $query->where('user_id', auth()->user()->id);
+            })
+            ->where('reservation_start', '>=', Carbon::parse(today()))
+            ->where('checkin', 1)
+            ->where('seats_available', '>', 0)
+            ->where('status', 1)
+            ->orderBy('reservation_start', 'asc')
+            ->get()
+            ->groupBy(function ($booking) {
+                return $booking->slot->title . ' - ' . Carbon::createFromFormat('d/m/Y H:i', $booking->reservation_start)->isoFormat('dddd, DD MMMM YYYY');
+            });
+
+        return view('home', compact('statistics', 'bookingDates', 'checkinDates', 'userMedicalGoingDue'));
     }
 
 }
