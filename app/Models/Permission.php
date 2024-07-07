a933f7966a5e05684c0267f788edec05efb7e92e
Neves Parrottino <neppoz@hotmail.com>
2024-07-07 22:20:05 +0200

    Added Models

---
+++ app/Models/Permission.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Permission extends Model
+{
+    use SoftDeletes;
+
+    const notification_all_users_medical = 115;
+    const notification_all_users_balance = 116;
+    public $table = 'permissions';
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'title',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
