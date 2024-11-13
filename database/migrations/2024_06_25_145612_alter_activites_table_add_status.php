d5a5eb11765c1fb72f84728155da542893d09642
Neves Parrottino <neppoz@hotmail.com>
2024-11-13 17:28:49 +0100

    small change on migration

--- database/migrations/2024_06_25_145612_alter_activites_table_add_status.php
+++ database/migrations/2024_06_25_145612_alter_activites_table_add_status.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('activities', function (Blueprint $table) {
-            $table->enum('status', ['new', 'approved'])->default('new');
+            $table->enum('status', [\App\Enums\ActivityStatus::New, \App\Enums\ActivityStatus::Approved])->default(\App\Enums\ActivityStatus::Approved);
         });
     }
 
