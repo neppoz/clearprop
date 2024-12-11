c78f54ed73d83b3b77f734a6e4a311873d5d147f
Neves Parrottino <neppoz@hotmail.com>
2024-12-11 12:01:19 +0100

    migrations User model fix

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
-use App\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
