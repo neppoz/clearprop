4220c0fbf7d145493d2ae68c557e2edd00d024cd
Neves Parrottino <neppoz@hotmail.com>
2024-12-12 10:20:50 +0100

    cleanup views

--- resources/views/partials/datatablesAdminCustomActions.blade.php
+++
@@ -1,8 +0,0 @@
-@can($undeleteGate)
-    <form action="{{ route('admin.' . $crudRoutePart . '.undelete', $row->id) }}" method="POST"
-          onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-        <input type="hidden" name="_method" value="POST">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <button type="submit" class="btn btn-xs btn-info">{{trans('global.datatables.undelete')}}</button>
-    </form>
-@endcan
--- resources/views/partials/logo.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="login-logo">
-    <div class="login-logo">
-        <img src="{{ url('/images/ClearProp_textdown.svg') }}" alt="ClearProp Logo" width="150" height="auto"/>
-    </div>
-</div>
