8735778d25fc5e0a2bcfccfcb0038ab09d0c73cb
Neves Parrottino <neppoz@hotmail.com>
2024-12-28 11:46:31 +0100

    fix ViewPage

--- app/Filament/Resources/ReservationResource/Pages/ViewReservation.php
+++ app/Filament/Resources/ReservationResource/Pages/ViewReservation.php
@@ -3,10 +3,22 @@
 namespace App\Filament\Resources\ReservationResource\Pages;
 
 use App\Filament\Resources\ReservationResource;
+use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewReservation extends ViewRecord
 {
     protected static string $resource = ReservationResource::class;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['reservation_start_date'] = Carbon::createFromFormat('Y-m-d H:i:s', $data['reservation_start'])->format('Y-m-d');
+        $data['reservation_start_time'] = Carbon::createFromFormat('Y-m-d H:i:s', $data['reservation_start'])->format('H:i');
+        $data['reservation_stop_date'] = Carbon::createFromFormat('Y-m-d H:i:s', $data['reservation_stop'])->format('Y-m-d');
+        $data['reservation_stop_time'] = Carbon::createFromFormat('Y-m-d H:i:s', $data['reservation_stop'])->format('H:i');
+        $data['status'] = 1;
+
+        return $data;
+    }
 }
