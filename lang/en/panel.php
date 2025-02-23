e9fead35ac46fff81e50471e2bfcf5e0859c378f
Neves Parrottino <neppoz@hotmail.com>
2025-02-23 11:20:24 +0100

    lang enhancement

--- lang/en/panel.php
+++ lang/en/panel.php
@@ -15,7 +15,7 @@ return [
     'totalAirtime' => 'Total Airtime',
     'avgDuration' => 'Avg. per Mission',
     'loggedMissions' => 'Logged Missions',
-    'depositTotal' => 'Deposit Total',
+    'depositTotal' => 'Balance',
     'last_6_months' => 'last 6 Months',
 
 ];
